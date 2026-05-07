156cbc6318f89e28c6dd6dbae71a3f9fdc65f199
neoforean <voieon@proton.me>
2026-05-07 02:07:48 +0200

    Add files via upload

--- tool.js
+++ tool.js
@@ -1,5 +1,17 @@
+const toolHolder = document.getElementById('tool-holder');
+const toolFrame = document.querySelector('#tool-holder > div > iframe');
+const toolHolderDiv = document.querySelector('#tool-holder > div');
+
 document.querySelectorAll("#tools > div").forEach(element => {
     element.onclick = () => {
-        document.location = document.location + element.textContent + "/" + "index.html"
+        toolFrame.src = document.location + element.textContent + "/" + "index.html"
+        toolHolder.style.visibility = 'visible';
+        toolHolderDiv.classList.add('popin');
     }
 });
+
+document.querySelector(".titlebar > button").onclick = () => {
+    toolFrame.src = "";
+    toolHolder.style.visibility = 'collapse';
+    toolHolderDiv.classList.remove('popin');
+}
